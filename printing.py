af05bafa62a88be4c7c16b036ad67b4a28b5b54c
tabebqena@gmail.com <tabebqena@gmail.com>
2023-01-08 10:37:44 +0300

    improve printing

--- printing.py
+++ printing.py
@@ -9,17 +9,19 @@ def print_car(car_dict):
         print(k.title(), "        :", v)
 
 
-def center_string_in_column(string, width):
+def center_string_in_space(string, space_width):
+    string = string.strip()
     string_length = len(string)
-    left_pad = int((width - string_length) / 2)
-    right_pad = width - string_length - left_pad
+    remain = space_width - string_length
+    left_pad = int(remain / 2)
+    right_pad = space_width - string_length - left_pad
     return (" " * left_pad) + string + (" " * right_pad)
 
 
 def print_car_as_row(car_dict, fields=[], column_widths={}, min_column_width=10):
     txt = ""
     for field_name in fields:
-        txt += "|" + center_string_in_column(
+        txt += "|" + center_string_in_space(
             car_dict.get(field_name), column_widths.get(field_name, min_column_width)
         )
     txt += "|"
@@ -30,7 +32,7 @@ def print_car_as_row(car_dict, fields=[], column_widths={}, min_column_width=10)
 def print_header_line(fields, widths):
     txt = ""
     for field_name in fields:
-        txt += "|" + center_string_in_column(field_name.title(), widths[field_name])
+        txt += "|" + center_string_in_space(field_name.title(), widths[field_name])
     txt += "|"
     length = len(txt)
     print("=" * length)
@@ -59,35 +61,44 @@ def print_car_list(car_list, fields):
         print("-" * len(line))
 
 
-def print_message_in_window(msg, width=100, char="#"):
-    print("f")
+def convert_message_to_lines_list(message, line_width):
     lines = []
-    msg = msg.strip()
+    msg = message.strip()
     length = len(msg)
-    if length + 2 <= width:
-        msg = " " + msg + " "
+    if length <= line_width:
         lines.append(msg)
-
     else:
         words = msg.split()
-        line = " "
+        line = ""
         for word in words:
+            word = word.strip() + " "
             word_length = len(word)
-            print(line)
-            if len(line) + word_length + 2 <= width:
-                line = line + word + " "
+            if len(line) + word_length <= line_width:
+                line = line + word
             else:
                 lines.append(copy(line))
-                print(lines)
-                line = " " + word + " "
+                line = word
         if line:
             lines.append(copy(line))
-    print(char * (width + 2))
-    for line in lines:
-        line_length = len(line)
-        remain = width - line_length
-        left_pad = int(remain / 2)
-        right_pad = width - line_length - left_pad
+    return lines
 
-        print(char + (" " * left_pad) + line + (" " * right_pad) + char)
-    print(char * (width + 2))
+
+def print_message_in_window(msg, width=100, char="#"):
+    available_width = width - 4  # because we will add 4 cagaracters (2 spaces & 2 char)
+    lines = convert_message_to_lines_list(msg, available_width)
+    print(char * width)
+    for line in lines:
+        print(char + " " + center_string_in_space(line, available_width) + " " + char)
+    print(char * width)
+
+
+def print_messages_in_window(messages, width=100, char="#"):
+    lines = []
+    available_width = width - 4  # because we will add 4 cagaracters (2 spaces & 2 char)
+
+    for message in messages:
+        lines.extend(convert_message_to_lines_list(message, available_width))
+    print(char * width)
+    for line in lines:
+        print(char + " " + center_string_in_space(line, available_width) + " " + char)
+    print(char * width)
